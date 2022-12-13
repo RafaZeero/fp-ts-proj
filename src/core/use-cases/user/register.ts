837ffb779eb82b2f4ee33612a9c94e76806ab6f2
Rafael Lima <rafaa99@gmail.com>
2022-12-12 23:09:22 -0300

    fix prettier 2.0

--- src/core/use-cases/user/register.ts
+++ src/core/use-cases/user/register.ts
@@ -3,11 +3,11 @@ import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/lib/function'
 import { CreateUser } from '@/core/types/user'
 
-export type OutsideRegister<A> = (data: CreateUser) => Promise<A>;
+export type OutsideRegister<A> = (data: CreateUser) => Promise<A>
 
 type Register = <A>(
   outsideRegister: OutsideRegister<A>
-) => (data: CreateUser) => TE.TaskEither<Error, A>;
+) => (data: CreateUser) => TE.TaskEither<Error, A>
 
 export const register: Register = (outsideRegister) => (data) => {
   return pipe(TE.tryCatch(() => outsideRegister(data), E.toError))
